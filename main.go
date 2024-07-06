16bf79c11845e0b7548c70e2e275f48ca2d015ba
Ross Heat <rossalanheaton@gmail.com>
2024-07-06 19:53:36 +0100

    Merge update functions

--- main.go
+++ main.go
@@ -4,9 +4,10 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/charmbracelet/bubbles/spinner"
 	"github.com/charmbracelet/bubbles/textinput"
-
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 type ErrMsg error
@@ -31,22 +32,29 @@ func (s Route) String() string {
 
 type Model struct {
 	TextInput textinput.Model
+	Spinner   spinner.Model
 	Err       error
 	Route     Route
 }
 
 func InitialModel() Model {
+	// TextInput
 	ti := textinput.New()
 	ti.Placeholder = "Is the square root of 441 greater than 20?"
 	ti.Focus()
 	ti.CharLimit = 256
 	ti.Width = 50
 
-	return Model{Err: nil, TextInput: ti, Route: QuestionRoute}
+	// Spinner
+	s := spinner.New()
+	s.Spinner = spinner.Dot
+	s.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+
+	return Model{Err: nil, TextInput: ti, Route: QuestionRoute, Spinner: s}
 }
 
 func (m Model) Init() tea.Cmd {
-	return textinput.Blink
+	return tea.Batch(m.Spinner.Tick, textinput.Blink)
 }
 
 type LLMResults struct {
@@ -54,81 +62,48 @@ type LLMResults struct {
 	noCount  int
 }
 
-func AskQuestion() tea.Msg {
+func (m Model) AskQuestion() tea.Msg {
 	time.Sleep(time.Second * 5)
 	return LLMResults{}
 }
 
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-
-	switch m.Route {
-	case QuestionRoute:
-		return m.QuestionUpdate(msg)
-	case LoadingRoute:
-		return m.LoadingUpdate(msg)
-	case ResultsRoute:
-		return m.ResultsUpdate(msg)
-	}
-	return m, nil
-}
-
-func (m Model) QuestionUpdate(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
 
 	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.Type {
-		case tea.KeyCtrlC, tea.KeyEsc:
-			return m, tea.Quit
-		case tea.KeyEnter:
-			m.Route = LoadingRoute
-			return m, AskQuestion
-		}
-	case ErrMsg:
-		m.Err = msg
-		return m, nil
-	}
-	m.TextInput, cmd = m.TextInput.Update(msg)
-	return m, cmd
-}
 
-func (m Model) LoadingUpdate(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.Type {
-		case tea.KeyCtrlC, tea.KeyEsc:
-			return m, tea.Quit
-		}
+	case spinner.TickMsg:
+		var cmd tea.Cmd
+		m.Spinner, cmd = m.Spinner.Update(msg)
+		return m, cmd
 
 	case LLMResults:
 		m.Route = ResultsRoute
 		return m, nil
 
-	case ErrMsg:
-		m.Err = msg
-		return m, nil
-	}
-	return m, nil
-}
-
-func (m Model) ResultsUpdate(msg tea.Msg) (tea.Model, tea.Cmd) {
-
-	switch msg := msg.(type) {
 	case tea.KeyMsg:
-		switch msg.Type {
-		case tea.KeyCtrlC, tea.KeyEsc:
+		switch msg.String() {
+		case "q":
 			return m, tea.Quit
-		case tea.KeyEnter:
-			m.Route = QuestionRoute
-			m.TextInput.Reset()
-			return m, nil
+		case "enter":
+			if m.Route == QuestionRoute {
+				m.Route = LoadingRoute
+				return m, m.AskQuestion
+			}
+		case "r":
+			if m.Route == ResultsRoute {
+				m.TextInput.Reset()
+				m.Route = QuestionRoute
+				return m, nil
+			}
 		}
 	case ErrMsg:
 		m.Err = msg
 		return m, nil
 	}
 
-	return m, nil
+	m.TextInput, cmd = m.TextInput.Update(msg)
+	return m, cmd
 }
 
 func (m Model) View() string {
@@ -146,19 +121,21 @@ func (m Model) View() string {
 }
 
 func (m Model) QuestionView() string {
-	return fmt.Sprintf(
-		"Ask the LLM a yes/no question:\n\n%v\n\n%v",
+	return fmt.Sprint(
+		"Ask the LLM a yes/no question:",
+		"\n\n",
 		m.TextInput.View(),
-		"(esc to quit)",
+		"\n\n",
+		"(q to quit)",
 	) + "\n"
 }
 
 func (m Model) LoadingView() string {
-	return "Loading..."
+	return fmt.Sprint("\n", m.Spinner.View(), "Asking the the LLM: ", m.TextInput.Value(), "\n\n", "(q to quit)")
 }
 
 func (m Model) ResultsView() string {
-	return fmt.Sprint("Results: y:20, n:10", "\n\n", "(Press Enter to reset)")
+	return fmt.Sprint("Results: y:20, n:10", "\n\n", "(r to reset, q to quit)")
 }
 
 func main() {
